a15a9a7eebfe5f6fc6ceed3839e6b39ecc74123e
ssaini19 <143258109+ssaini19@users.noreply.github.com>
2023-08-26 15:22:33 +0000

    Completed Final Css Style

--- app.py
+++ app.py
@@ -2,10 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+REPORTS = [
+  {
+    'id': 1,
+    'site': 'Christies Beach',
+    'title': 'Clinical Indicators',
+    'Month': 'August',
+  },
+  {
+    'id': 2,
+    'site': 'Casa Mia',
+    'title': 'National Quality Clinical Indicators',
+    'Month': 'August',
+  },
+  {
+    'id': 3,
+    'site': 'Edge Hill',
+    'title': 'Monthly Clinical Indicators',
+    'Month': 'August',
+  },
+]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                         reports=REPORTS,
+                         company_name='Insight Ai')
 
 
 if __name__ == "__main__":
